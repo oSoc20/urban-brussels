c0b279911434c26906d4b7ba098d0d8b955599ea
chloedevriese <chloe.devriese@student.howest.be>
2020-07-08 16:55:30 +0200

    Made the markers clickable

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import "./Map.css";
 import mapboxgl from 'mapbox-gl';
 import markerBuilding from './assets/icons/building_marker.png';
@@ -8,6 +8,14 @@ mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
 const Map = () => {
     const mapContainerRef = useRef(null);
+    const [markerIsClicked, setIsClicked ] = useState(false);
+    const [selectedBuilding, setSelectedBuilding ] = useState({
+        'city': null,
+        'street': null,
+        'number': null,            
+        'img': null 
+    });
+
 
     //Initialize map after render
     useEffect(() => {
@@ -21,7 +29,7 @@ const Map = () => {
         map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
 
         map.on('load', () => {
-            map.loadImage(markerBuilding, ( error, image)=> {
+            map.loadImage(markerBuilding, ( error, image) => {
               if (error) throw error;
               map.addImage('markerBuilding', image);
             });
@@ -49,9 +57,66 @@ const Map = () => {
               'icon-size': 0.1
             }, 
         });
+
+        map.on('click', (e) => {setIsClicked(false);});
+      
+
+        map.on('click', 'buildingsJette', e => {
+            setIsClicked(true);
+
+            setSelectedBuilding({
+                'city': e.features[0].properties.CITIES_NL,
+                'street': e.features[0].properties.STREET_NL,
+                'number': e.features[0].properties.NUMBER,
+                'img': e.features[0].properties.FIRSTIMAGE
+            })
+        });
+
+        map.on('mouseenter', 'buildingsJette', () => {
+            map.getCanvas().style.cursor = 'pointer';
+        });
+             
+        map.on('mouseleave', 'buildingsJette', () => {
+            map.getCanvas().style.cursor = '';
+        });
     }
 
-    return <div className="mapContainer" ref={mapContainerRef} />;
+    
+    return (
+        <div>
+            <div className='sidebarStyle'> <div>Jette </div> </div>
+            <div className="mapContainer" ref={mapContainerRef} />;
+            <div className= {"detail-popup " + (markerIsClicked ? 'shown' : '')}>
+                <div className="small-version">
+                    <div className="scrolling">
+                        <div className="city">{selectedBuilding.city}</div>
+                        <div className="street">{selectedBuilding.street} {selectedBuilding.number}</div>
+                        <div className="thumbnail">
+                          <img src={selectedBuilding.img} alt="" className="img-responsive" />
+                        </div>
+                        <div className="description">
+                           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer semper vitae est feugiat laoreet. 
+                           Ut eu placerat mi. In hac habitasse platea dictumst. Duis commodo vitae lacus in maximus. 
+                           Donec nisl sem, bibendum nec nisi eget, viverra bibendum purus. Etiam tincidunt viverra lacus, 
+                           vel sodales sapien. Aliquam sed erat dui. Nunc non malesuada turpis, eget viverra tellus. 
+                        </div>
+                        <div className="square-button read-more">Read More</div>
+                        <div className="tags-title">Style tags</div>
+                        <div className="tags">
+                            <span className="tag">Baroque</span>
+                            <span className="tag">Romanesque</span>
+                            <span className="tag">Bauhaus</span>
+                            <span className="tag">Neo-classical</span>
+                            <span className="tag">Renaissance</span>
+                            <span className="tag">Gothic</span>
+                            <span className="tag">Modernist</span>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
 };
 
 export default Map;
