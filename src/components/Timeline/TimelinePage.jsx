e18b931381df1a2684c4b60a575bc213c272a5ed
dbtruong <duybao.truong@student.vinci.be>
2020-07-09 10:23:56 +0200

    update: images in public

--- src/components/Timeline/TimelinePage.jsx
+++ src/components/Timeline/TimelinePage.jsx
@@ -6,7 +6,7 @@ const dates = ["5 january 1923", "26 april 1970", "14 december 2018"];
 const names = ["Eiffel Tower","Collégiale Saints-Pierre-et-Guidon", "Taj Mahal"]
 const streetName =["Rue de Praetere 24", "Boulevard Anspach 153-153a-153b", "Rue de la Vénerie 54-56-58"]
 const architects = ["Jean VAN RUYSBROECK", "J.-J. VAN YSENDIJCK", "Georges HAUPT"]
-const pictures = ["/src/assets/pic1.jpg","/images/pic2.jpg","/images/pic3.jpg"]
+const pictures = ["/images/pic1.jpg","/images/pic2.jpg","/images/pic3.jpg"]
 
 const Timeline = () => {
   const [value, setValue] = useState(0);
