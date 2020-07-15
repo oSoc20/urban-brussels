bad8d2be547c0bfd60aa64215d62b3eeca983b17
chloedevriese <chloe.devriese@student.howest.be>
2020-07-15 16:06:59 +0200

    Added image and address to building detail page

--- src/components/detailBuilding.js
+++ src/components/detailBuilding.js
@@ -3,7 +3,22 @@ import mapboxgl from 'mapbox-gl'
 const DetailBuilding = {
   render: async () => {
     const view = /* html */`
-    <div id="mapContainer" class="mapContainer"></div>
+    <div id="mapContainer" class="map-building-detail"></div>
+    <section class= "detail-popup  is-visible">
+    <div class="detail-popup__container">
+      <div class="detail-popup__overflow">
+        <div class="detail-popup__row">
+          <img class="detail-popup__img" src=https://monument.heritage.brussels/medias/500/buildings/10900023/10900023_0007_P01.jpg alt="" />
+          
+          <div class="detail-popup__address">
+            <h1 class="detail-popup__address__street"> Capartlaan 7 </h1>
+            <h1 class="detail-popup__address__municipality">1090 Jette</h1>
+          </div>
+        </div>
+      </div>
+   
+    </div>
+  </section>
 
           `
     return view
