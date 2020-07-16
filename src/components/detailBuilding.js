09ac19c4e9a0cd5a47716a63f93f46b0fd9c2136
chloedevriese <chloe.devriese@student.howest.be>
2020-07-16 11:36:20 +0200

    Added animated marker on the map

--- src/components/detailBuilding.js
+++ src/components/detailBuilding.js
@@ -83,6 +83,93 @@ const DetailBuilding = {
     })
 
     map.addControl(new mapboxgl.NavigationControl(), 'bottom-right')
+
+    const size = 200
+
+    const pulsingDot = {
+      width: size,
+      height: size,
+      data: new Uint8Array(size * size * 4),
+
+      onAdd: function () {
+        const canvas = document.createElement('canvas')
+        canvas.width = this.width
+        canvas.height = this.height
+        this.context = canvas.getContext('2d')
+      },
+
+      render: function () {
+        const duration = 2000
+        const t = (performance.now() % duration) / duration
+
+        const radius = (size / 2) * 0.3
+        const outerRadius = (size / 2) * 0.7 * t + radius
+        const context = this.context
+
+        context.clearRect(0, 0, this.width, this.height)
+        context.beginPath()
+        context.arc(
+          this.width / 2,
+          this.height / 2,
+          outerRadius,
+          0,
+          Math.PI * 2
+        )
+        context.fillStyle = 'rgba(33, 33, 68,' + (1 - t) + ')'
+        context.fill()
+
+        context.beginPath()
+        context.arc(
+          this.width / 2,
+          this.height / 2,
+          radius,
+          0,
+          Math.PI * 2
+        )
+        context.fillStyle = 'rgba(33, 33, 68, 1)'
+        context.strokeStyle = 'white'
+        context.lineWidth = 2 + 4 * (1 - t)
+        context.fill()
+        context.stroke()
+
+        this.data = context.getImageData(
+          0,
+          0,
+          this.width,
+          this.height
+        ).data
+
+        map.triggerRepaint()
+        return true
+      }
+    }
+
+    map.on('load', function () {
+      map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 })
+      map.addSource('points', {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: [
+            {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [4.32120602, 50.88532209]
+              }
+            }
+          ]
+        }
+      })
+      map.addLayer({
+        id: 'points',
+        type: 'symbol',
+        source: 'points',
+        layout: {
+          'icon-image': 'pulsing-dot'
+        }
+      })
+    })
   }
 }
 
